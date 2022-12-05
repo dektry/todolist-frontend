3bac46683186529c05d438a3b50622be97129243
Shkuratov Artem <shuratov2001@mail.ru>
2022-12-05 13:35:30 +0300

    added eslint/hasky/prettier and google cloud yml

--- src/pages/Home/HomeContainer.tsx
+++ src/pages/Home/HomeContainer.tsx
@@ -2,45 +2,43 @@ import React, { useState, useEffect } from 'react';
 
 import { useSnackbar } from 'notistack';
 
-import { HomeUI } from './HomeUI'
+import { HomeUI } from './HomeUI';
 
-import { useFetchAllToDosQuery, useDeleteTodoMutation, useCreateToDoMutation} from '../../api/toDoListApi'
+import { useFetchAllToDosQuery, useDeleteTodoMutation, useCreateToDoMutation } from '../../api/toDoListApi';
 
 export function HomeContainer() {
-  const { 
-          isError: isToDoListError, 
-          isLoading: isListLoading,
-          data: toDoList, 
-          isFetching: isListFetching
-        } = useFetchAllToDosQuery({limit: 10, page: 0, isCompleted: false})
-  const [deleteToDo, {isLoading: isDeleteLoading, isError: isDeleteError}] = useDeleteTodoMutation()
-  const [createToDo, {isLoading: isCreateLoading, isError: isCreateError}] = useCreateToDoMutation()
-  
-  
-  const isLoading = isListLoading || isListFetching || isDeleteLoading || isCreateLoading
-  const isError = isDeleteError || isCreateError || isToDoListError
+  const {
+    isError: isToDoListError,
+    isLoading: isListLoading,
+    data: toDoList,
+    isFetching: isListFetching,
+  } = useFetchAllToDosQuery({ limit: 10, page: 0, isCompleted: false });
+  const [deleteToDo, { isLoading: isDeleteLoading, isError: isDeleteError }] = useDeleteTodoMutation();
+  const [createToDo, { isLoading: isCreateLoading, isError: isCreateError }] = useCreateToDoMutation();
 
-  const [toDoInputText, setToDoInputText] = useState<string>('')
+  const isLoading = isListLoading || isListFetching || isDeleteLoading || isCreateLoading;
+
+  const [toDoInputText, setToDoInputText] = useState<string>('');
 
   const { enqueueSnackbar } = useSnackbar();
 
-  const handleChangeToDoInput = (e: React.FormEvent<HTMLInputElement>) =>{
-    setToDoInputText(e.currentTarget.value)
-  }
+  const handleChangeToDoInput = (e: React.FormEvent<HTMLInputElement>) => {
+    setToDoInputText(e.currentTarget.value);
+  };
 
   const handleClickAddToDo = () => {
-    if (!toDoInputText)  enqueueSnackbar('ToDo name cannot be empty!', {variant: 'error'})
-    else
-      createToDo({content: toDoInputText})
+    if (!toDoInputText) enqueueSnackbar('ToDo name cannot be empty!', { variant: 'error' });
+    else createToDo({ content: toDoInputText });
   };
 
   useEffect(() => {
-    if (isError) enqueueSnackbar('Opps, some error!', {variant: 'error'})
-  }, [isError])
+    if (isDeleteError || isCreateError || isToDoListError)
+      enqueueSnackbar('Opps, network error!', { variant: 'error' });
+  }, [isDeleteError, isCreateError, isToDoListError]);
 
   return (
-    <HomeUI 
-      toDoInputText={toDoInputText} 
+    <HomeUI
+      toDoInputText={toDoInputText}
       toDoList={toDoList}
       isLoading={isLoading}
       handleChangeToDoInput={handleChangeToDoInput}
@@ -49,4 +47,3 @@ export function HomeContainer() {
     />
   );
 }
-
